3214d3841e565f8aab92182a0b1d84e210de008a
nadine222012301 <149670421+nadine222012301@users.noreply.github.com>
2023-11-04 18:50:43 +0200

    Add files via upload

---
+++ ifelseex1.java
@@ -0,0 +1,15 @@
+
+public class ifelseex1 {  
+public static void main(String[] args) {  
+ 
+    int number=50;  
+     
+    if(number>50){  
+        System.out.println("pass");  
+    }else{  
+        System.out.println("fail");  
+    }  
+}  
+}   
+    
+
