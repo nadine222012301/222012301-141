4091415037e0725d81f033dfd0cb9675438e0ff9
nadine222012301 <149670421+nadine222012301@users.noreply.github.com>
2023-11-04 19:34:38 +0200

    Add files via upload

---
+++ switchcase3.java
@@ -0,0 +1,14 @@
+public class switchcase3{  
+    public static void main(String[] args) {   
+        int number=20;  
+ switch(number){  
+   case 10: System.out.println("30");  
+        break;  
+        case 20: System.out.println("1");  
+        break;  
+        case 30: System.out.println("5");  
+        break;  
+        default:System.out.println("Not in 30, 1or 5");  
+        }  
+    }  
+}
